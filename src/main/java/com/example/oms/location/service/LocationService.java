9cf1b4bcd2270bc7eef4a776e7ea5be682672667
Ishwar Shinde <shindeishwar08@gmail.com>
2025-12-24 19:25:20 +0530

    Stale Driver Filtering (The Sidecar Pattern)

--- src/main/java/com/example/oms/location/service/LocationService.java
+++ src/main/java/com/example/oms/location/service/LocationService.java
@@ -10,7 +10,9 @@ import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -21,11 +23,26 @@ public class LocationService {
 
     private final RedisTemplate<String, String> redisTemplate;
     private static final String GEO_KEY = "oms:partner:locations";
+    private static final long STALE_THRESHOLD_MS = 600000;
 
     public void savePartnerLocation(Long partnerId, double lat, double lon) {
-        // Redis expects (Longitude, Latitude)
-        Point point = new Point(lon, lat);
-        redisTemplate.opsForGeo().add(GEO_KEY, point, partnerId.toString());
+        String memberId = String.valueOf(partnerId);
+        
+        String dataKey = "oms:partner:data:" + partnerId; 
+
+        redisTemplate.executePipelined(new SessionCallback<Object>() {
+            @Override
+            public Object execute(RedisOperations operations) {
+                // Command 1: Geo Index
+                operations.opsForGeo().add(GEO_KEY, new Point(lon, lat), memberId);
+
+                // Command 2: Metadata Sidecar
+                operations.opsForHash().put(dataKey, "lastUpdated", String.valueOf(System.currentTimeMillis()));
+                operations.opsForHash().put(dataKey, "isAvailable", "true");
+
+                return null;
+            }
+        });
     }
 
     public Point getPartnerLocation(Long partnerId) {
@@ -51,7 +68,42 @@ public class LocationService {
             partnerIds.add(result.getContent().getName());
         }
 
-        return partnerIds;
+        return filterStalePartners(partnerIds);
 
     }
+
+    public List<String> filterStalePartners(List<String> partnerIds){
+        if(partnerIds.isEmpty()) return partnerIds;
+
+        List<Object> timestamps = redisTemplate.executePipelined(new SessionCallback<Object>() {
+
+            // Pipeline: Fetch "lastUpdated" for ALL partners in one go
+            public Object execute(RedisOperations operations){
+                for(String id:partnerIds){
+                    operations.opsForHash().get("oms:partner:data:"+id, "lastUpdated");
+                }
+                return null;
+            }
+        });
+
+        // Java Side: Filter out the old ones
+        Long currTime = System.currentTimeMillis();
+        List<String> validPartners = new ArrayList<>();
+
+
+        for(int i=0;i<partnerIds.size();i++){
+            Object time = timestamps.get(i);
+
+            if(time !=null){
+                try {
+                    long updatedTime = Long.parseLong((String)time);
+                    if(currTime-updatedTime<STALE_THRESHOLD_MS) validPartners.add(partnerIds.get(i));
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+        return validPartners;
+    }
+
+    
 }
